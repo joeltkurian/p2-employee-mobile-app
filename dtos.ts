88e4f2be2267f7caf4506e020ce08d58473ef6d2
Joel Kurian <joeltkurian@gmail.com>
2022-02-03 15:02:28 -0600

    added dtos

---
+++ dtos.ts
@@ -0,0 +1,50 @@
+export interface Activity {
+    id: string,
+    title: string,
+    desc: string,
+    startTime: number,
+    endTime: number,
+    location: string,
+    status: "On Schedule" | "Cancelled"
+}
+
+export interface ServiceRequest {
+    id: string,
+    room: string,
+    created: number,
+    status: "Ordered" | "Processing" | "Completed" | "Cancelled",
+    requestedOffering: Offering[]
+}
+
+export interface Offering {
+    desc: string,
+    cost: number
+}
+
+export interface ProblemSubmission {
+    desc: string,
+    base64Photo?: string
+}
+
+export interface Problem {
+    id: string,
+    submittedTime: number,
+    desc: string,
+    status: "Unreviewed" | "Reviewed",
+    photoLink?: string
+}
+
+export interface Employee {
+    id: number,
+    isManager: boolean,
+    fname: string,
+    lname: string,
+    username: string,
+    password: string
+}
+
+export interface WorkLog {
+    wId: number,
+    type: "CHEKIN" | "CHECKOUT",
+    timestamp: number
+}
