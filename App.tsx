6d9d3e8f47e1d8df09807f5c62e7af416532fedf
Daniel J Belenski <38082687+Belesnkid@users.noreply.github.com>
2022-02-08 20:48:08 -0500

    component skeleton
    skeleton of component established

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RoomService from './room-service/room-service-component';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <RoomService/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
